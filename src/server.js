69060fa3de68fb02c19a52ff197e80b15e1c8e6e
Ryan Conry <r.conry@hotmail.com>
2020-12-27 18:15:06 -0500

    Added concurrently to run FE and BE in tandem.  Update server for cors and access to mongoDB atlas

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const GraphQLSchema = require('./schemas/GraphQLSchema');
 const { rootResolvers } = require('./resolvers');
 
-mongoose.connect('mongodb://localhost:27017/exercises', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://burlyboi:burlyboi@cluster0.jm4nl.mongodb.net/exercises?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.once('open', () => {
   console.log('conneted to database');
 });
 
 const app = express();
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema: GraphQLSchema,
   rootValue: rootResolvers,
